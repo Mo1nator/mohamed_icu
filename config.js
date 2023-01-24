3445aa29413268d60ea8e112dcd7dbf0a26932ea
Mo1nator <119340423+Mo1nator@users.noreply.github.com>
2023-01-25 01:28:40 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+window.PorfolioConfig = {
+  name: 'Mohamed Yassin',
+  jobTitle: 'Computer Science Student',
+  jobDescription: 'Currently a computer science student at Modern Academy',
+  skills: [
+  {
+      name: 'Fast Learner',
+      description: 'Can pickup new skills relatively fast'
+    },
+    {
+      name: 'Problem Solving',
+      description: '100+ problems solved on Codeforces with a rating of 750+'
+    },
+	{
+      name: 'C++ Programming',
+      description: 'Intermediate level C++ programmer'
+    },
+	{
+      name: 'Linux Programming',
+      description: 'Daily driving Linux and highly familiar with CLI environments and bash scripts'
+    },
+	{
+      name: 'English Fluency',
+      description: 'Through years of continous training'
+    }
+  ],
+
+
+  contacts: {
+    email: 'moinator856@gmail.com',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/Mo1nator'
+      },
+	  {
+        name: 'Codeforces',
+        link: 'https://codeforces.com/profile/Moinator'
+      },
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/mohamed-yassin-45825a22a/'
+      }
+    ]
+  }
+};
